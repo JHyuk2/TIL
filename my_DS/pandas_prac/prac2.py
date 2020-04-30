213b0e8328411401c124dc64a60bfff6365c7421
JHyuk2 <whdgurdl77@gmail.com>
2020-04-30 22:57:01 +0900

    Add pracs

---
+++ my_DS/pandas_prac/prac2.py
@@ -0,0 +1,32 @@
+a = 0.7**12
+b = (0.7**11) * 0.3 * 12
+c = (0.7**10) * 0.3**2 * 6*11
+
+# print(1 - round((a+b), 6))
+
+n = 25
+p = 0.3
+a = 0.8643
+
+import math
+# print(0.16 * 0.25**2 * 0.29**2 * 30)
+b = (15*14*13)/(20*19*18)
+# print(4.25*(51/76))
+print('here')
+print(0.6026 - (1-0.6879))
+# print(20 / math.sqrt(24))
+
+def nCr(n,r):
+    f = math.factorial
+    return f(n) / f(r) / f(n-r)
+
+a = nCr(40, 8)
+b = nCr(60, 12)
+c = nCr(100, 20)
+# print(a*b/c)
+ab = nCr(20, 8)
+d = 0.4**8
+e = 0.6**12
+# print(ab*d*e)
+E = math.e
+print(17.2 - 3.8**2)
